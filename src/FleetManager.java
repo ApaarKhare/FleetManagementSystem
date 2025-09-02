63543fa9a4f718adbfb33219d25e2b51bfd4b78e
Apaar Khare <apaarkhare@gmail.com>
2025-09-03 03:03:22 +0530

    FleetManager created, prolly signing off for the day

---
+++ src/FleetManager.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+public class FleetManager {
+    private List<Vehicle> fleet;
+
+    void addVehicle(Vehicle v){
+
+    }
+
+    void removeVehicle(String id) throws InvalidOperationException{
+
+    }
+
+    void startAllJourneys(double distance){
+
+    }
+
+    double getTotalFuelConsumption(double distance){
+
+    }
+
+    void maintainAll(){
+
+    }
+
+    void sortFleetByEfficiency(){
+
+    }
+
+    List<Vehicle> searchByType(Class<?> type){
+
+    }
+
+    String generateReport(){
+
+    }
+
+    List<Vehicle> getVehiclesNeedingMaintenance(){
+
+    }
+}
